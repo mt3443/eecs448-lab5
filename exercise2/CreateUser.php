8f1f496eb1e447afb90a9acef5d2fa021f12b6fd
Matthew Taylor <mjt@ku.edu>
2017-10-09 14:00:51 -0500

    Implemented backend php

--- exercise2/CreateUser.php
+++ exercise2/CreateUser.php
@@ -1,5 +1,33 @@
 <?php
 
+	$userIDs = new mysqli("mysql.eecs.ku.edu", "mtaylor", "P@$$word123", "Users");
+	
+	if($userIDs->connect_errno) {
+		printf("Connetion failed: %s\n", $userIDs->connect_error);
+		exit();
+	}
+	
+	
 	echo "<html><body>";
 	
-	$input
+	$username = $_POST["username"];
+	
+	if(strlen($username) == 0) {
+		echo "Error! Username cannot be blank";
+		exit();
+	}
+	
+	$query = "SELECT * FROM Users WHERE user_id='" + $username + "'";
+	
+	if($userIDs->query($query)) {
+		echo "Error! The username, " + $username + ", is already in use";
+		exit();
+	} else {
+		$insert = "INSERT INTO Users (" + $username + ");";
+		$userIDs->query($insert);
+		echo "Username successfully added!";
+	}
+	
+	$userIDs->close();
+	
+?>
